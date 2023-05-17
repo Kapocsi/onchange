b75e7a037387e28617d5c1069744909949dcccc6
Thomas Kapocsi <tomkap011@gmail.com>
2023-05-16 19:53:01 -0600

    v1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::args;
+use std::fs::read;
+use std::time::Duration;
+
+fn main() -> Result<(), ()> {
+    let mut old_file: Vec<u8> = Vec::new();
+    let mut first_run = true;
+    dbg!(&old_file);
+    loop {
+        let args: Vec<String> = args().collect();
+
+        if let Some(file_path) = args.get(1) {
+            if let Ok(file) = read(file_path) {
+                if old_file != file && !first_run {
+                    break;
+                }
+                first_run = false;
+                old_file = file;
+            } else {
+                println!("Err Failed to read file");
+                break;
+            }
+        } else {
+            println!("Err No file providied");
+            break;
+        }
+
+        std::thread::sleep(Duration::from_secs(1))
+    }
+
+    Ok(())
 }
